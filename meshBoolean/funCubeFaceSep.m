da1a39e6a1cb4a968ed903969d56f3dcd0fd125b
zhou436 <zyj19931230@gmail.com>
2022-08-11 11:16:34 +0200

    meshBoolean update new Algo

---
+++ meshBoolean/funCubeFaceSep.m
@@ -0,0 +1,21 @@
+function [Boolean] = funCubeFaceSep(cubeCoor, TetCoor)
+% input cubeCoor:   A matrix (8,3), 8 points x,y,z
+% input TetCoor:    A matrix (4,3), 4 points x,y,z
+Boolean = false;
+maxCoor = max(cubeCoor,[],1); % maximum coordinates in x,y,z directions
+minCoor = min(cubeCoor,[],1); % minimum coordinates in x,y,z directions
+for ii=1: 1: 3
+    if TetCoor(ii,1) > maxCoor(ii) &&...
+            TetCoor(ii,2) > maxCoor(ii) &&...
+            TetCoor(ii,3) > maxCoor(ii) &&...
+            TetCoor(ii,4) > maxCoor(ii)
+        Boolean = true;
+        break
+    elseif TetCoor(ii,1) < minCoor(ii) &&...
+            TetCoor(ii,2) < minCoor(ii) &&...
+            TetCoor(ii,3) < minCoor(ii) &&...
+            TetCoor(ii,4) < minCoor(ii)
+        Boolean = true;
+        break
+    end
+end
---
+++ meshBoolean/test2.m
@@ -0,0 +1,81 @@
+clear all
+close all
+clc
+%% Example 01
+% figure(1)
+% clf
+fileName = 'Job-Boolean.inp';
+data = abaqusInpRead(fileName);
+%% plot nodes
+scatter3(data.Nodes{1,1}(:,2),data.Nodes{1,1}(:,3),data.Nodes{1,1}(:,4));
+axis equal
+hold on
+scatter3(data.Nodes{1,2}(:,2),data.Nodes{1,2}(:,3),data.Nodes{1,2}(:,4));
+axis equal
+%% plot hex mesh
+plotMesh(data.Elements{1,1}, data.Nodes{1,1});
+%% Screw geometry and preprocessing
+outerRadius = 2.0; % Outter Radius 2.0 mm
+innerRadius = 0.95; % Inner Radius 0.95 mm
+% loop over Hex mesh check if any point inner Outer diameter
+hexNodeNum = size(data.Nodes{1,1},1);
+hexNodeZone = [];
+for ii=1: 1: hexNodeNum
+    if (data.Nodes{1,1}(ii,2)^2+data.Nodes{1,1}(ii,3)^2) <= outerRadius^2
+        hexNodeZone = [hexNodeZone;data.Nodes{1,1}(ii,:)];
+    end
+end
+% scatter3(hexMeshZone(:,1),hexMeshZone(:,2),hexMeshZone(:,3));
+% loop over Hex mesh check if any point between Inner and Outer diameter
+hexNodeZoneNum = size(hexNodeZone,1);
+hexNodeBTZone = [];
+hexNodeInZone = [];
+for ii=1: 1: hexNodeZoneNum
+    if (hexNodeZone(ii,2)^2+hexNodeZone(ii,3)^2) >= innerRadius^2
+        hexNodeBTZone = [hexNodeBTZone;hexNodeZone(ii,:)];
+    else
+        hexNodeInZone = [hexNodeInZone;hexNodeZone(ii,:)];
+    end
+end
+hexNodeBTZoneNum = size(hexNodeBTZone,1);
+hexNodeInZoneNum = size(hexNodeInZone,1);
+ele2Del = [];
+for ii=1: 1: hexNodeInZoneNum
+    [row,~] = find(data.Elements{1,1}==hexNodeInZone(ii,1));
+    ele2Del = [ele2Del;row];
+end
+ele2Del = unique(ele2Del);
+% check the hex elements between inner and outer diameter
+hexEleBTZone = [];
+for ii=1: 1: hexNodeBTZoneNum
+    [row,~] = find(data.Elements{1,1}==hexNodeBTZone(ii,1));
+    hexEleBTZone = [hexEleBTZone;row];
+end
+hexEleBTZone = unique(hexEleBTZone);
+% Delete the inner zone hex elements from the between zone elements
+hexEleBTInZone = [];
+for ii=1: 1: size(ele2Del,1)
+    [row,~] = find(hexEleBTZone==ele2Del(ii));
+    hexEleBTInZone = [hexEleBTInZone;row];
+end
+hexEleBTInZone = unique(hexEleBTInZone);
+hexEleBTZone(hexEleBTInZone,:) = [];
+% hexEleBTZone = ;
+plotMesh(data.Elements{1,1}(hexEleBTZone,:), data.Nodes{1,1});
+%% Select the Tet elements contribute to region outer Innerdiameter
+tetNodeNum = size(data.Nodes{1,2},1);
+tetNodeZone = []; % the tet element nodes of screw outer innerdiamter
+for ii=1: 1: tetNodeNum
+    if (data.Nodes{1,2}(ii,2)^2+data.Nodes{1,2}(ii,3)^2) >= innerRadius^2
+        tetNodeZone = [tetNodeZone;data.Nodes{1,2}(ii,:)];
+    end
+end
+tetNodeZoneNum = size(tetNodeZone,1);
+tetEle2Check = []; % the tet elements of screw contain element nodes outer innerdiameter
+for ii=1: 1: tetNodeZoneNum
+    [row,~] = find(data.Elements{1,2}==tetNodeZone(ii,1));
+    tetEle2Check = [tetEle2Check;row];
+end
+tetEle2Check = unique(tetEle2Check);
+tetEle2CheckNode = data.Elements{1,2}(tetEle2Check,:);
+%% check if hex mesh points inside tet mesh elements
