e32edca22de15911df607d147d1dbb0d910b6aab
zhou436 <zyj19931230@gmail.com>
2022-08-22 13:50:50 +0200

    del demo 1 2, abaData as function separated

---
+++ voxelMesh/abaInpData.m
@@ -0,0 +1,46 @@
+function abaData = abaInpData()
+%% Abaqus parameters Bone
+abaData.Bone.MAT.matName = 'Bone';
+abaData.Bone.MAT.varDens = '1.89e-09';
+abaData.Bone.MAT.vaEL = [300, 0.3]; % Young's modulus and Poisson's ratio
+abaData.Bone.MAT.varCDPPlas = [40.0, 0.1, 1.16, 0.6667, 0.0]; % CDP plasticity table
+abaData.Bone.MAT.varCDPCHard = [... % CDP compressive hardening
+    6.0, 0.0;
+    8.0, 0.00833333;
+    10.0, 0.0166667;
+    6.75, 0.0441667;
+    3.5, 0.0716667;
+    0.25, 0.0991667;
+    ];
+abaData.Bone.MAT.varCDPTSti = [... % CDP tension stiffening
+    6.0, 0.0;
+    4.06667, 0.0164444;
+    2.13333, 0.0328889;
+    0.2, 0.0493333;
+    ];
+abaData.Bone.MAT.varCDPCDam = [... % CDP compression damage
+    0.0, 0.0;
+    0.0, 0.00833333;
+    0.0, 0.0166667;
+    0.325, 0.0441667;
+    0.65, 0.0716667;
+    0.975, 0.0991667;
+    ];
+abaData.Bone.MAT.varCDPTDam = [... % CDP tension damage
+    0.0, 0.0;
+    0.322222, 0.0164444;
+    0.644444, 0.0328889;
+    0.966667, 0.0493333;
+    ];
+% CDP failure strain and damage
+abaData.Bone.MAT.varCDPFai = [0.0493333, 0.0991667, 0.966667, 0.975];
+abaData.Bone.Parts.eleType = ['C3D8','C3D8R']; % element type, for printInp_multiSect % C3D8R reduced integration point
+abaData.Bone.Parts.partName = ['Bone','Void'];
+abaData.Bone.Parts.partNum = 2;
+
+%% Abaqus parameters General
+abaData.mSFactor = '1e-04';
+abaData.fricCoeef = 0.30;
+abaData.displacement = -2.5;
+
+end
