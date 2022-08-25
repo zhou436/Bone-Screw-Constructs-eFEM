08ef22f1dd1a26eb06b8bf65ac41e10a18838eb0
zhou436 <zyj19931230@gmail.com>
2022-08-25 14:20:16 +0200

    negative CDP plastic strain...TBD

--- voxelMesh/funCDPGen.m
+++ voxelMesh/funCDPGen.m
@@ -1,15 +1,15 @@
 function CDPModel = funCDPGen(MAT)
 MAT.EL = 10000;             % Elastic modulus
 MAT.comp.sigmaY = 100;      % compression yield stress [MPa]
-MAT.comp.sigmaUYD = 20;     % compression ultimate stress [MPa]
+MAT.comp.sigmaUYD = 0;     % compression ultimate stress [MPa]
 MAT.comp.sigmaU = MAT.comp.sigmaY + MAT.comp.sigmaUYD;
 MAT.comp.epsilonU = 0.02;   % compression ultimate strain [-]
 MAT.comp.sigmaF = MAT.comp.sigmaU * 0.05;   % compression failure(deletion) stress [MPa]
-MAT.comp.epsilonF = 0.1;    % compression failure (deletion) strain [-]
+MAT.comp.epsilonF = 0.05;    % compression failure (deletion) strain [-]
 
 MAT.tens.sigmaY = 100;        % compression yield stress [MPa]
 MAT.tens.sigmaF = MAT.tens.sigmaY * 0.05;     % compression failure(deletion) stress [MPa]
-MAT.tens.epsilonF = 0.05;    % compression failure (deletion) strain [-]
+MAT.tens.epsilonF = 0.025;    % compression failure (deletion) strain [-]
 %
 EL = MAT.EL;
 
@@ -34,12 +34,25 @@ faiCNum = ceil(sigmaUC*0.95/1);
 MAT.comp.CDPtable = zeros([elaCNum+yieCNum+faiCNum-2,6]);
 MAT.comp.CDPtable(1:elaCNum,2) = linspace(0,sigmaYC,elaCNum);
 MAT.comp.CDPtable(1:elaCNum,1) = linspace(0,epsilonYC,elaCNum);
+% yield stage
 MAT.comp.CDPtable(elaCNum:elaCNum+yieCNum-1,2) = linspace(sigmaYC,sigmaUC,yieCNum);
 MAT.comp.CDPtable(elaCNum:elaCNum+yieCNum-1,1) = linspace(epsilonYC,epsilonUC,yieCNum);
+% failure stage
 MAT.comp.CDPtable(elaCNum+yieCNum-1:elaCNum+yieCNum+faiCNum-2,2) = linspace(sigmaUC,sigmaFC,faiCNum);
 MAT.comp.CDPtable(elaCNum+yieCNum-1:elaCNum+yieCNum+faiCNum-2,1) = linspace(epsilonUC,epsilonFC,faiCNum);
+% damage
+MAT.comp.CDPtable(:,3) = MAT.comp.CDPtable(:,2)/sigmaUC;
+MAT.comp.CDPtable(1:elaCNum+yieCNum-1,3) = 0;
+% elastic strain
+MAT.comp.CDPtable(:,4) = MAT.comp.CDPtable(:,2)/EL;
+% inelastic strain
+MAT.comp.CDPtable(:,5) = MAT.comp.CDPtable(:,1)-MAT.comp.CDPtable(:,4);
+% plastic strain
+MAT.comp.CDPtable(:,6) = MAT.comp.CDPtable(:,5)-MAT.comp.CDPtable(:,3)./(1-MAT.comp.CDPtable(:,3))...
+    .*MAT.comp.CDPtable(:,2)/EL;
 
-plot(MAT.comp.CDPtable(:,1),MAT.comp.CDPtable(:,2));
+plot(MAT.comp.CDPtable(:,6));
+
 
 % create table for tension
 MAT.tens.strainArr = [];
